e5100f5b1cad34abedea0dbaf2b440359e9c498f
qwanzip <102371201+qwanzip@users.noreply.github.com>
2022-03-29 08:45:23 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "a9aa59ac-05ca-491b-89d1-0ce721079e49",
                         "alterId": 0
                     }
                 ],
